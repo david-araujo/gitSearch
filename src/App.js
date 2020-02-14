171dac0a5396bc7fcb6a716ba03fded8e7a55ca6
David Araujo <david.araujo-ext@duratex.com.br>
2020-02-14 11:29:31 -0300

    Styled serach field and solved issue when search languages with # char.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+import { Dropdown } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 
 function App() {
@@ -15,11 +17,18 @@ function App() {
     const response = await fetch('https://api.github.com/languages');
     const data = await response.json();
 
-    setLanguages(data);
+    const list = data.reduce((acc, currentValue) => {
+      let alias = currentValue.aliases[0].replace('#', 'sharp')
+      acc.push({key: alias, value: alias, text: currentValue.name})
+      return acc
+    }, [])
+
+    setLanguages(list);
   }
   
   const getRepositories = useCallback(
     async () => {
+      console.log(typeof language)
       const response = await fetch(`https://api.github.com/search/repositories?q=language:${language}&sort=stars&order=desc&page=${pageNum}`)
       const data = await response.json();
 
@@ -39,16 +48,25 @@ function App() {
   return (
     <>
     <Header />
-    <section className="App">      
-      <select onChange = {(e) => {setLanguage(e.target.value); setPageNum(1)}}>
-        <option value = ''>Selecione uma Linguagem</option>
-        {languages.map(lang => (
-          <option key = {lang.name} value = {lang.name}>{lang.name}</option>
-        ))}
-      </select>
+    <section className="App">
+      <Dropdown 
+        placeholder = 'Selecione uma Linguagem'
+        search
+        selection 
+        options = {languages} 
+        onChange = 
+        {
+          (e, target) => {
+            setLanguage(target.value)
+            setPageNum(1)
+          }
+        } 
+      />
       <ul>
         {repositories.map(repo => (
-          <li key = {repo.id}>{repo.name}</li>
+          <li key = {repo.id}>
+            {repo.name}
+          </li>
         ))}
       </ul>
       {(repositories.length < 30 && pageNum < 2) || pageNum < 2 ? null : <button onClick = {() => setPageNum(pageNum - 1)}>PREV</button> }
